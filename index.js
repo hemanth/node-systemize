3dcefca5bc56ca2c585274bf0e0d7447fc8a6932
hemanth.hm <hemanth.hm@gmail.com>
2014-06-27 08:12:28 +0530

    First cut.

--- index.js
+++ index.js
@@ -1,38 +1,78 @@
-module.exports = function(){
-	var globby = require('globby');
+module.exports = function(dir, cb) {
+    'use strict';
+    if (!cb) {
+        throw new Error('Callback is a must!');
+    }
+    var globby = require('globby');
     var fs = require('fs-extra');
-    var mv = require('mv');
+    var join = require('path').join;
 
-	var imgs = [
-		'*.jpg',
-		'*.png',
-		'*.gif'
-	];
+    var imgs = [
+        '*.jpg',
+        '*.png',
+        '*.gif'
+    ];
 
-	var docs = [
-		'*.txt',
-		'*.pdf',
-		'*.docs',
-		'*.pdf',
-		'*.ppt',
-		'*.odt',
-		'*.xls',
-		'*.odt'
-	];
+    var docs = [
+        '*.txt',
+        '*.pdf',
+        '*.docs',
+        '*.pdf',
+        '*.ppt',
+        '*.odt',
+        '*.xls',
+        '*.odt'
+    ];
 
-	var music = [
-		'*.mp3',
-		'*.wav',
-		'*.oga'
-	];
+    var music = [
+        '*.mp3',
+        '*.wav',
+        '*.oga',
+        '*.mp4'
+    ];
 
-	globby(imgs, function (err, paths) {
-		paths.forEach(function(path){
-			path = path.replace('\\','/');
-			console.log(path);
-			fs.rename(path, "images",function(err){
-				console.log(err);
-			});
-		});
+    var organise = function(args) {
+        globby(args.type, function(err, paths) {
+            if (err) {
+                cb(err, null);
+            }
+            if (paths.length > 1) {
+                fs.ensureDir(args.dir, function(err) {
+                    if (err) {
+                        cb(err, null);
+                    }
+                    paths.forEach(function(path) {
+                        fs.move(path, join(args.dir, '/' + path), function(err) {
+                            if (err) {
+                                cb(err, null);
+                            }
+                        });
+                    });
+                    cb(null, 'Done!');
+                });
+            }
+        });
+    };
+
+    var pwd = process.cwd();
+
+    if (!fs.existsSync(dir)) {
+        cb('dir not found!', null);
+    }
+    process.chdir(dir);
+    organise({
+        type: imgs,
+        dir: 'images'
     });
-}
+    organise({
+        type: music,
+        dir: 'music'
+    });
+    organise({
+        type: docs,
+        dir: 'documents'
+    });
+
+    process.chdir(pwd);
+
+};
